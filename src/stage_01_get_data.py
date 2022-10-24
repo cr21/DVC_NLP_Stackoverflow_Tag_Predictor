8b69d85a5dc55abd47ecc4e521d0f65da9e16d4f
cr21 <cr.tagadiya@gmail.com>
2022-10-24 13:31:53 -0400

    stage1 data ingestion from s3 done

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -1,14 +1,14 @@
 import argparse
 import os
-import shutil
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml, create_directories
-import random
+from src.utils.common import create_directories, create_directories, read_yaml
+import urllib.request as req
 
 
-STAGE = "STAGE_NAME" ## <<< change stage name 
+STAGE = "stage 01 get data" ## <<< change stage name 
 
+# set logging level Info, logging file format and in append mode
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
     level=logging.INFO, 
@@ -17,24 +17,33 @@ logging.basicConfig(
     )
 
 
-def main(config_path, params_path):
+def main(config_path):
     ## read config files
     config = read_yaml(config_path)
-    params = read_yaml(params_path)
-    pass
+    source_data_url = config['source_data_url']
+    local_data_dir = config['source_download_dir']['data_dir']
+    # create local directories where data will be put
+    create_directories([local_data_dir])
+    data_filename = config['source_download_dir']['data_file']
+    # create local file data path
+    local_data_file_path = os.path.join(local_data_dir,data_filename)
+    logging.info("Download started")
+    # get data from url
+    filename, headers = req.urlretrieve(source_data_url, local_data_file_path)
+    logging.info("Download finished")
+    logging.info(f"Download file is present at {filename}")
+    logging.info(f"Download header  {headers}")
 
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
-    args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
-        main(config_path=parsed_args.config, params_path=parsed_args.params)
-        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+        main(config_path=parsed_args.config)
     except Exception as e:
         logging.exception(e)
         raise e
