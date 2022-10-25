b6bcf5f875432170d7e7f616238b611efd6fda26
cr21 <cr.tagadiya@gmail.com>
2022-10-25 19:18:44 -0400

    Fixed Stage 03 error

--- src/utils/featuerize.py
+++ src/utils/featuerize.py
@@ -8,7 +8,7 @@ import numpy as np
 def save_matrix(df: pd.DataFrame,out_file_path:str, tf_idf_matrix: sparse.csr_matrix):
     logging.info("Creating sparse combined matrix")
     id_matrix = sparse.csr_matrix(df.id.astype(np.int64)).T
-    label_matrix = sparse.csr_matrix(df.id.astype(np.int64)).T
+    label_matrix = sparse.csr_matrix(df.label.astype(np.int64)).T
     result = sparse.hstack([id_matrix, label_matrix, tf_idf_matrix], format="csr")
     joblib.dump(result, out_file_path)
     msg =f"The output matrix saved at  {out_file_path} of size {result.shape} and data type {result.dtype}"
